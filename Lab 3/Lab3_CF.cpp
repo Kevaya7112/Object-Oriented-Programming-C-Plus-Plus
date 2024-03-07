2e1fdbb96cf023c2bc85e1077d762321ccec5392
Kevaya7112 <161499335+Kevaya7112@users.noreply.github.com>
2024-03-07 14:47:55 -0800

    Add files via upload

---
+++ Lab 3/Lab3_CF.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+// practice if/else and while statements//
+int main() {
+  
+int x = 0;
+int y = 5;
+
+while(x < y){
+  printf("%i/n",x);
+  x=x+3;
+}
+if( x % 2 ==0){
+  printf("x is even");}
+  else{ printf("x is odd");}
+}
